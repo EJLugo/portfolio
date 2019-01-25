f4bd9af8b21e52b7aa4e7ef2ec795bd59a649b64
Evelyn Lugo <evelynlugo@Evelyns-MacBook-Air.local>
2019-01-25 13:06:21 -0500

    WIP project page

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,17 @@
-console.log('hey there');
-
 //PROJECTS PAGE
 function aboutProject(project){
   const description = [
     {
       name: 'Quarto',
       description: 'A two player game with 16 pieces.',
-      screenshot: 
     },
     {
       name: 'Spot: Best Furry Friend',
-      description: 'Spot: Best Friend Finder is a website that uses Petfinders API to help users in NYC find a dog to adopt.'
+      description: 'Spot: Best Friend Finder is a website that uses Petfinders API to help users in NYC find a dog to adopt.',
     },
     {
       name: 'Exquisite Chronicle',
-      description: 'Exquisite Chronicle allows users to create an account and add to a prompt to create a story along with other users.'
+      description: 'Exquisite Chronicle allows users to create an account and add to a prompt to create a story along with other users.',
     }
   ]
   const h1tag = $('.project-name');
